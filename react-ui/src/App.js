4811c2b3730a8618f4603d946e70bcaffe1b93ff
Mark Oliver <mark.c.oliver@gmail.com>
2019-01-02 18:44:18 -0500

    installed redux

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,6 +3,34 @@ import './App.css';
 import Header from './components/HeaderComponents/Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      fetching: true,
+    };
+  }  
+  componentDidMount() {
+    fetch('/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({
+          message: json.message,
+          fetching: false,
+        });
+      })
+      .catch((e) => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false,
+        });
+      });
+  }  
   render() {
     return (
       <div className="App">
