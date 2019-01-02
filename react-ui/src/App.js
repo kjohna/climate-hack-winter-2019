a366fe988ece29350c36164590e04cd0b39763c3
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-02 16:43:32 -0700

    Body component created

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/HeaderComponents/Header';
+import Body from './components/BodyComponents/Body'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Body />
       </div>
     );
   }
