cd8b621babfab7f6643bbb3f8c411e92c8ee2d11
Henry Neal <henry.neal0@gmail.com>
2019-01-03 15:37:07 -0500

    alert flag comp added

--- react-ui/src/components/WeatherAlertsComponents/WeatherAlerts.js
+++ react-ui/src/components/WeatherAlertsComponents/WeatherAlerts.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import './WeatherAlerts.css';
 
+
+const dskey = "f39109d1263aa6b04983e1c34ef1e3df";
+const gmapkey = "AIzaSyCd8iUXQ2EfQF-LktLoJzgJ4toV7Q8ZW40";
+const zip = "07960";
+
+const simplesky = require("simplesky");
+var weather = new simplesky(gmapkey, dskey);
+
+
 class WeatherAlerts extends Component {
+  
+  componentDidMount() {
+    const getSummary = () => {
+      weather.getWeather(zip, null, null, ['alerts', 'flags']).then((response) => {
+        console.log(response);
+    }).catch((error) => {
+        console.log(error);
+    });
+    };
+    getSummary();
+  }
+
   render () {
-    const zip = 10001;
 
     return (
       <div className="weather-alerts">
         <form action="">
-          <h4>This is a Weather Alert!</h4>
-          <input type="text" ></input>
+
+          <h4>This is a Weather Alert! Powered by Dark Sky</h4>
+
+          <p>Light rain tomorrow through Tuesday, with high temperatures bottoming out at 38°F on Monday.</p>
         </form>
       </div>
     );
   }
 }
 
+// document.write('')
 export default WeatherAlerts;
