ee050677df3e6cafbb2176038ec7c9091b39eea7
Mark Oliver <mark.c.oliver@gmail.com>
2019-01-03 12:58:44 -0500

    redux, axios

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+var dotenv = require('dotenv');
+dotenv.load();
 
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
+const weatherEndpoints = require('./endpoints/weatherEndPoints');
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -20,23 +27,32 @@ if (!isDev && cluster.isMaster) {
   });
 
 } else {
-  const app = express();
-
+  const server = express();
+  server.use(bodyParser.json());
+  server.use(morgan('common'));
+  server.use(helmet());  
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
+  server.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  server.use(
+    cors({
+      credentials: true,
+      origin: [process.env.CLIENT_URL],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    })
+  );
+  server.use('/api/weather', weatherEndpoints);
   // Answer API requests.
-  app.get('/api', function (req, res) {
+  server.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
+  server.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 
-  app.listen(PORT, function () {
+  server.listen(PORT, function () {
     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
   });
 }
