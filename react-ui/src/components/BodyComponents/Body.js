ca3bc70c3dd75ac0dd2e45c2dc4a06730c99e9ca
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-02 17:06:13 -0700

    LocationSearch component created

--- react-ui/src/components/BodyComponents/Body.js
+++ react-ui/src/components/BodyComponents/Body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Body.css';
-import DateSelector from '../DateSelector/DateSelector';
+import DateSelector from '../DateSelectorComponents/DateSelector';
+import LocationSearch from '../LocationSearchComponents/LocationSearch';
 
 class Body extends Component {
   render() {
@@ -11,6 +12,7 @@ class Body extends Component {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at risus egestas, ultricies turpis sed, pulvinar lorem. Fusce placerat lobortis massa, vitae scelerisque sem convallis non. Phasellus ac magna ac quam scelerisque condimentum. Cras eget lorem a felis volutpat elementum eget id turpis. In vitae mi quis justo consectetur tincidunt. Etiam commodo vel velit sit amet auctor. Nulla imperdiet congue ipsum, id euismod lorem lacinia ac. Nulla vitae purus in diam tincidunt aliquam id vel nulla. Integer imperdiet sollicitudin elementum. Mauris non hendrerit tellus, iaculis hendrerit diam.
         </p>
         <DateSelector />
+        <LocationSearch />
       </div>
     );
   }
