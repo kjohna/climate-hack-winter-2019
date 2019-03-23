2c9922ee27d063d047a225e28bb1496dfe65f287
Mark Oliver <mark.c.oliver@gmail.com>
2019-03-22 23:51:23 -0400

    server only

--- react-ui/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import temperatureReducer from './temperature';
-const rootReducer = combineReducers({temps: temperatureReducer})
-
-export default rootReducer;
